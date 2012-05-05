d83433cf0318f0e399e3e9b12d5c37aae507ae3d
Lowell Kirsh <lowellk@gmail.com>
2012-05-04 22:16:55 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pretty-print-url',
+    description='Command line tool to pretty print urls.',
+    version='0.1',
+    url='https://github.com/lowellk/pretty-print-url',
+    author='Lowell Kirsh',
+    author_email='lowellk@gmail.com',
+    py_modules=['pretty-print-url']
+)
