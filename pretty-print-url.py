27e512cc6011874c3a0106227f62998b29fed7a9
Lowell Kirsh <lowellk@gmail.com>
2012-05-04 22:46:52 -0700

    fix bugs and distribute properly

--- pretty-print-url.py
+++ pretty-print-url.py
@@ -12,23 +12,25 @@ def pretty_print_url(url_string):
     """pretty print an url to stdout"""
     parsed = urlparse(url_string)
     query = parse_qs(parsed.query)
-    max_key_len = max([len(key) for key in query.keys()])
-
     print 'scheme:   %s' % parsed.scheme
     print 'hostname: %s' % (parsed.hostname or '')
     print 'port:     %s' % (parsed.port or '')
     print 'path:     %s' % parsed.path
     print 'params:   %s' % parsed.params
-    print 'query:'
     print 'fragment: %s' % parsed.fragment
-    for k, v in query.items():
-        if len(v) is 1:
-            v = v[0]
-        print '  %s: %s' % (k.ljust(max_key_len), v)
+
+    query_keys = [len(key) for key in query.keys()]
+    if query_keys:
+        max_key_len = max(query_keys)
+        print 'query:'
+        for k, v in query.items():
+            if len(v) is 1:
+                v = v[0]
+            print '  %s: %s' % (k.ljust(max_key_len), v)
 
 if __name__ == '__main__':
     if len(sys.argv) is not 2:
-        print >> sys.stderr, sys.argv[0]
+        print >> sys.stderr, 'Usage: %s <url>' % sys.argv[0]
         sys.exit(1)
 
     pretty_print_url(sys.argv[1])
--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup
 setup(
     name='pretty-print-url',
     description='Command line tool to pretty print urls.',
-    version='0.1',
+    version='0.2',
     url='https://github.com/lowellk/pretty-print-url',
     author='Lowell Kirsh',
     author_email='lowellk@gmail.com',
-    py_modules=['pretty-print-url']
+    scripts=['pretty-print-url.py']
 )
